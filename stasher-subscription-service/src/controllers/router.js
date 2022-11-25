ef663e65fdd7e61738a45109be26066c8ad07e2d
Joseln <joselencr@gmail.com>
2022-11-25 11:42:56 -0300

    add subscription and unsubscription endpoints

---
+++ stasher-subscription-service/src/controllers/router.js
@@ -0,0 +1,15 @@
+const Router = require('koa-router');
+const SubscriptionController = require("./subscriptionController");
+const subscriptionController = new SubscriptionController();
+const router = new Router();
+
+router.post("/api/subscription/user", async (ctx, next) => {
+    await subscriptionController.subscribe(ctx, next)
+});
+
+router.delete("/api/subscription/category/:category/user/:email", async (ctx, next) => {
+    await subscriptionController.unsubscribe(ctx, next)
+});
+
+
+module.exports = router;
